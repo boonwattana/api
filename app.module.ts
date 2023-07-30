addd9069e4beda1ebfc60816e5e1a334d9c00483
Mong <mongmang.vak@gmail.com>
2023-07-30 11:13:32 +0700

    change

--- app.module.ts
+++ app.module.ts
@@ -89,7 +89,7 @@ import { WordModule } from './core/word/word.module';
     // ),
     TypeOrmModule.forRoot({
       type:'postgres',
-      url:'localhost',
+      url:'127.0.0.1',
       port:5432,
       username:'dbuser',
       password:"pEM8F^hZBHuMfJYK",
