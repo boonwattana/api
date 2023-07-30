3510428827ac87fe72ead34aa8f0786a8b5b1670
Mong <mongmang.vak@gmail.com>
2023-07-30 15:21:08 +0700

    edit

--- core/authentications/authentications.module.ts
+++ core/authentications/authentications.module.ts
@@ -23,14 +23,12 @@ import { YearTermModule } from "src/api/year-term/year-term.module";
       property: "users",
       session: false,
     }),
-    JwtModule.registerAsync({
-      imports: [ConfigService],
-      inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => ({
-        secret: configService.get("JWT_SECRET_KEY"),
-        signOptions: { expiresIn: "7d" },
-      }),
+    JwtModule.register({
+      secret:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjcyIiwidXNlcm5hbWUiOiJURVNUQ1JFQVRFNSIsImZpcnN0TmFtZSI6IlRlc3RlciIsImxhc3ROYW1lIjoiVGVzdGVyIiwidHlwZUNvZGUiOiIiLCJkZXBhcnRtZW50Q29kZSI6IiIsImlhdCI6MTY1NDc0MzI1NywiZXhwIjoxNjU1MzQ4MDU3fQ.mBz_ugVyFBcdN9F1KYzBkq36sBtQrkFmTDNHiU9pm84',
+      signOptions:{ expiresIn: "7d" }
+
     }),
+
     TypeOrmModule.forFeature([Student,ActiveTime,Teacher
       ]),
       ClassroomModule,
